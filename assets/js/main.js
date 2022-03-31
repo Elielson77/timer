68640cd8b02096d429909f3b517300b76ca0696f
Elielson77 <elielsonll77@gmail.com>
2022-03-31 08:42:45 -0300

    conclui o js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,34 @@ function criaSegundos(segundos){
     timeZone: 'GMT'
   });
 }
-console.log(criaSegundos(1))
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
-let segundos = 0; 
+let segundos = 1; 
+let temp;
+
+function iniciaTime(){
+  temp = setInterval(function(){
+    relogio.innerHTML = criaSegundos(segundos)
+    segundos++;
+  }, 1000);
+}
+console.log(criaSegundos(segundos));
+
+iniciar.addEventListener('click', function(e){
+  relogio.classList.remove('pausado')
+  clearInterval(temp)
+  iniciaTime();
+})
+
+pausar.addEventListener('click', function(e){
+  clearInterval(temp)
+  relogio.classList.add('pausado');
+})
+
+zerar.addEventListener('click', function(e){
+  segundos = 1; 
+  relogio.innerHTML = '00:00:00';
+})
